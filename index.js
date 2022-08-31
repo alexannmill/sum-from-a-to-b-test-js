d85547907bfef6b5b363e3f475a02f477495e34b
alexannmill <alexannmill@gmail.com>
2022-08-31 15:55:01 +0000

    finished sum with recu

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+// function sum(fromN, toN) {
+// //base case
+// let counter = fromN
+// if (fromN === toN){
+//   return counter
+// }else {
+//   counter= counter + fromN
+//   return sum(fromN + 1, toN)
+// }
+// };
+console.log(sum( 5, 7))
+// //for loop
+//   let counter = 0
+//   for(i = formN; i=< toN; i++){
+//     counter ++
+//   }
 
 module.exports = sum;
+
+
+function sum(fromN, toN) {
+  //base case
+    if (fromN === toN){
+      return fromN
+    }else {
+      return fromN + sum(fromN +1, toN)
+    }
+  };
+   sum( 5, 10)
